253d2ba613126d693d934bd7f86395764176e130
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2025-11-19 23:10:44 +0530

    i start arraylist today

--- DSA_By_Self/src/DSAbyShraddha/ArrayList/ArrayList_Cret.java
+++ DSA_By_Self/src/DSAbyShraddha/ArrayList/ArrayList_Cret.java
@@ -4,6 +4,41 @@ import java.util.ArrayList;
 
 public class ArrayList_Cret {
 
+
+    public void reversList(ArrayList<Integer> list){
+        for (int i= list.size()-1; i>=0;i--){
+
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println(" ");
+
+    }
+
+    public void findMax(ArrayList<Integer> list){
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<list.size();i++){
+            if(max<list.get(i)){
+                max= list.get(i);
+
+            }
+            System.out.println("maximum element is  " + max);
+        }
+    }
+    public void swapNO(ArrayList<Integer> list, int inx1,  int inx2 ) {
+
+
+        for (int i = 0; i < list.size(); i++) {
+
+            int temp= list.get(inx1);
+            list.set(inx1, list.get(inx2));
+            list.set(inx2,temp);
+
+
+            System.out.println();
+
+        }
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
@@ -16,5 +51,16 @@ public class ArrayList_Cret {
 
         list.remove(2);
         System.out.println(list);
+
+        list.set(2,5);
+        System.out.println(list);
+
+        ArrayList_Cret lst=new ArrayList_Cret();
+        lst.reversList(list);
+        lst.findMax(list);
+        lst.swapNO(list,0,1);
+
+
+
     }
 }
