4f68b9bd4c6d1d0cd1a551508818cf21bd3b91a0
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2026-01-07 10:48:38 +0530

    transvers each node

--- DSA_By_Self/src/DSAbyShraddha/Linked_List/impl_singly_LL.java
+++ DSA_By_Self/src/DSAbyShraddha/Linked_List/impl_singly_LL.java
@@ -2,8 +2,16 @@ package DSAbyShraddha.Linked_List;
 
 public class impl_singly_LL {
 
-    void print_LL(){
+    static void Transvers_LL(Node head){
+        while (head!=null){
+            System.out.print(head.data);
+            if (head.next!=null){
+                System.out.print("-->");
 
+            }
+            head=head.next;
+        }
+        System.out.println("");
 
     }
 
@@ -12,6 +20,10 @@ public class impl_singly_LL {
         head.next =new Node(20);
         head.next.next=new Node(30);
         head.next.next.next=new Node(40);
+        Transvers_LL(head);
+
+
+
 
         while (head!=null){
             System.out.print(head.data+" ");
