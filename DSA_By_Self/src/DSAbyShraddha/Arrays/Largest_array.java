1a36f600cb9470b470e2244064c5c5c9c05f00d9
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2026-02-06 16:07:19 +0530

    add recursive method in array to find max some array

--- DSA_By_Self/src/DSAbyShraddha/Arrays/Largest_array.java
+++ DSA_By_Self/src/DSAbyShraddha/Arrays/Largest_array.java
@@ -23,6 +23,25 @@ public class Largest_array {
 
     }
 
+    static int findMax(int[] arr, int i) {
+
+        // Last index returns the element
+        if (i == arr.length - 1) {
+            return arr[i];
+        }
+
+        // Find the maximum from the rest of the array
+        int recMax = findMax(arr, i + 1);
+
+        // Compare with i-th element and return
+        return Math.max(recMax, arr[i]);
+    }
+
+    static int largest(int[] arr) {
+        return findMax(arr, 0);
+    }
+
+
     public static void main(String[] args) {
         int numbers[] ={23,45,67,78,54};
         Largest_array obj = new Largest_array();
