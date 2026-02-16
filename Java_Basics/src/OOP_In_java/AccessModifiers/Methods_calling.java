341b44d2e88fe81b2d369da62bff9a9085f01e18
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2026-02-17 00:44:55 +0530

    java methods also

---
+++ Java_Basics/src/OOP_In_java/AccessModifiers/Methods_calling.java
@@ -0,0 +1,31 @@
+package OOP_In_java.AccessModifiers;
+
+public class Methods_calling {
+
+    public static void D() {
+        float d = 40.5f;
+        System.out.println("In Method D");
+    }
+
+    public static void C() {
+        double c = 30.5;
+        System.out.println("In Method C");
+    }
+
+    public static void B() {
+        int b = 20;
+        C(); // Calling C
+        System.out.println("In Method B");
+    }
+
+    public static void A() {
+        int a = 10;
+        B(); // Calling B
+        System.out.println("In Method A");
+    }
+
+    public static void main(String[] args) {
+        A(); // Start with function A
+        D(); // Then call D
+    }
+}
