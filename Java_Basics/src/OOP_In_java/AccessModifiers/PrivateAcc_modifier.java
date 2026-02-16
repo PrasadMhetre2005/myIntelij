1c5d7d7157ca23cc5289a3c1fc7bcd75f251ac21
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2026-02-17 00:18:16 +0530

    starting Modifiers cosept in java

---
+++ Java_Basics/src/OOP_In_java/AccessModifiers/PrivateAcc_modifier.java
@@ -0,0 +1,27 @@
+package OOP_In_java.AccessModifiers;
+
+class Person {
+
+    // private variable
+    private String name;
+
+    public void setName(String name)  {
+
+        this.name = name; // accessible within class
+    }
+
+    public String getName() { return name; }
+}
+
+public class PrivateAcc_modifier {
+    public static void main(String[] args)
+    {
+
+        Person p = new Person();
+        p.setName("Alice");
+
+        // System.out.println(p.name); // Error: 'name'
+        // has private access
+        System.out.println(p.getName());
+    }
+}
