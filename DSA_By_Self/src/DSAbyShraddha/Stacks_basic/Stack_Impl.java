9314a93ec93f7d4fdc48dadcbe00ff32de1a23e0
PrasadMhetre2005 <dipchandmhetre6010@gmail.com>
2026-02-27 10:39:53 +0530

    Procced stack

---
+++ DSA_By_Self/src/DSAbyShraddha/Stacks_basic/Stack_Impl.java
@@ -0,0 +1,44 @@
+package DSAbyShraddha.Stacks_basic;
+
+
+import java.util.Objects;
+import java.util.Stack;
+
+class stack_basics{
+
+
+
+    public static void Traverse_In_Stack(Stack<Integer> stack){
+
+
+        for (int num: stack){
+
+            System.out.println(num);
+        }
+    }
+
+}
+
+public class Stack_Impl {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack= new Stack<>();
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.peek());
+
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack);
+
+
+
+        stack_basics.Traverse_In_Stack(stack);
+
+
+    }
+}
